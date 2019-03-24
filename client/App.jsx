b0732be3a2c89541f229cf454f4672d2d31471b6
Chang Yea Moon <changyeamoon@gmail.com>
2019-03-23 17:53:28 -0700

    use react router to dynamically render main

--- client/App.jsx
+++ client/App.jsx
@@ -1,117 +1,49 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
-import SignIn from './components/SignIn';
+import React, { useState } from 'react';
+import { GoogleLogin } from 'react-google-login';
+import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom';
+import styled from 'styled-components';
+import Nav from './components/Nav';
 import Main from './components/Main';
-import ComposeMessage from './components/ComposeMessage';
-import ConfirmMessage from './components/ConfirmMessage';
-import ErrorMessage from './components/ErrorMessage';
-import D3Container from './containers/D3Container';
 
-// Components
+const Container = styled.div`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      loggedIn: false,
-      displayCompose: false,
-      messagesSent: [],
-      messagesInTransit: [],
-      messagesRecieved: [],
-      messageBody: '',
-      messageTo: '',
-      messageTitle: '',
-      messageFrom: '',
-      messageLocation: '',
+const App = () => {
+  const [login, setLogin] = useState(false);
 
-      pigeons: [{}],
-      increment: 0,
-    };
-  }
-
-  componentWillMount() {
-    fetch('/api/getAllUserPigeons', {
-      method: 'POST',
-      cache: 'no-cache',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ user_id: 2 }),
-    })
-      .then(res => res.json())
-      .then(pigeons => {
-        this.setState({ pigeons: pigeons });
-      });
-  }
-  changeMessage = e => {
-    if (e.target.id === 'message-title') {
-      this.setState({ messageTitle: e.target.value });
-    }
-    if (e.target.id === 'message-to') {
-      this.setState({ messageTo: e.target.value });
-    }
-    if (e.target.id === 'message-body') {
-      this.setState({ messageBody: e.target.value });
-    }
+  const responseGoogle = response => {
+    console.log('after login', response);
+    setLogin(true);
   };
-  incrementPigi = () => {
-    if (this.state.increment + 1 >= this.state.pigeons.length) {
-      this.setState({ increment: 0 });
-    } else {
-      this.setState({ increment: this.state.increment + 1 });
-    }
-  };
-  click = () => {
-    this.setState({ loggedIn: !this.state.loggedIn });
-  };
-  sendMessage() {
-    fetch('/api/sendMessage', {
-      method: 'POST',
-      cache: 'no-cache',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        user_sending_id: 2,
-        user_receiving_id: 1,
-        email_address: 'chang@gamil.com',
-      }),
-    })
-      .then(res => res.json())
-      .then(pigeons => {
-        this.setState({ pigeons: pigeons });
-      });
-  }
 
-  render() {
-    return (
-      <BrowserRouter>
+  return (
+    <Router>
+      <Container>
+        <Nav />
         <Switch>
           <Route
             exact
             path="/"
             render={() =>
-              this.state.loggedIn ? (
-                <Main
-                  displayPigi={this.state.pigeons[this.state.increment]}
-                  incrementPigi={this.incrementPigi}
-                />
+              login ? (
+                <Main />
               ) : (
-                <SignIn click={this.click} />
+                <GoogleLogin
+                  clientId={process.env.GOOGLE_CLIENT_ID}
+                  buttonText="Login"
+                  onSuccess={responseGoogle}
+                  onFailure={responseGoogle}
+                />
               )
             }
           />
-          <Route
-            path="/compose"
-            render={() => <ComposeMessage changeMessage={this.changeMessage} />}
-          />
-          <Route path="/confirm" render={() => <ConfirmMessage statey={this.state} />} />
-          <Route path="/map" component={D3Container} />
-          <Route component={ErrorMessage} />
         </Switch>
-      </BrowserRouter>
-    );
-  }
-}
+      </Container>
+    </Router>
+  );
+};
 
 export default App;
---
+++ client/components/Nav.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const DropContent = styled.div`
+  display: none;
+  position: relative;
+  background-color: #f9f9f9;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+  padding: 12px 16px;
+  z-index: 4;
+`;
+
+const DropDown = styled.div`
+  position: fixed;
+  bottom: 33%;
+  right: 1em;
+  z-index: 3000;
+
+  &:hover {
+    background-color: blue;
+  }
+  &:hover ${DropContent} {
+    display: block;
+  }
+`;
+
+const StyledLink = styled(Link)`
+  color: black;
+  font-weight: bold;
+  padding: 20px;
+`;
+
+const Nav = () => (
+  <DropDown>
+    <span>HELLO</span>
+    <DropContent>
+      <StyledLink to="/">home</StyledLink>
+      <StyledLink to="/account">about</StyledLink>
+      <StyledLink to="/pigeons">docs</StyledLink>
+    </DropContent>
+  </DropDown>
+);
+
+export default Nav;
